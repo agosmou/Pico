eb1ea3b3dcecb7380aa695c6b9b08314fbbb170a
ale_WSL2 <agosmou@gmail.com>
2022-11-25 15:43:25 -0800

    added unique key at line 40

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,36 +1,22 @@
 import React, {useEffect, useContext} from 'react';
 import RestaurantFinder from '../apis/RestaurantFinder';
-//import { RestaurantsContext } from '../context/RestaurantsContext';
+import { RestaurantsContext } from '../context/RestaurantsContext';
 
 
 
-// const RestaurantList = (props) => {
-//     const { restaurants, setRestaurants } = useContext(RestaurantsContext);
-//     useEffect(() => {
-//       const fetchData = async () => {
-//         try {
-//           const response = await RestaurantFinder.get("/");
-//           setRestaurants(response.data.data.restaurants);
-//         } catch (err) {}
-//       };
-  
-//       fetchData();
-//     }, []);
-
-
-const RestaurantList = () => {
-
+const RestaurantList = (props) => {
+    const { restaurants, setRestaurants } = useContext(RestaurantsContext);
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await RestaurantFinder.get("/");
-                console.log(response);
-              } catch (err) {}
-        };
-      
-
+      const fetchData = async () => {
+        try {
+          const response = await RestaurantFinder.get("/");
+          setRestaurants(response.data.data.restaurants);
+        } catch (err) {}
+      };
+  
       fetchData();
     }, []);
+
 
   return (
     <div className="list-group">
@@ -45,45 +31,47 @@ const RestaurantList = () => {
                     <th scope="col">Delete</th>
                 </tr>
             </thead>
-            <tbody>
-                {/* {restaurants?.map((restaurant) => { // Added the question mark after the variable for optional chaining 
 
-                    return (
-                        <tr>
-                            <td>{restaurant.name}</td>
-                            <td>{restaurant.location}</td>
-                            <td>{"$".repeat(restaurant.price_range)}</td>
-                            <td>reviews</td>
-                            <td>
-                                <button className="btn btn-warning">Update</button>
-                            </td>
-                            <td>
-                                <button className="btn btn-danger">Delete</button>
-                            </td>
-                        </tr>
-                    )
+                <tbody>
+                    {/* use return when using the map function */}
+                    {restaurants && restaurants.map((restaurant) => { 
+                        return (
+                            // added the below unique key prop per warning message
+                            <tr key={restaurant.id}> 
+                                <td>{restaurant.name}</td>
+                                <td>{restaurant.location}</td>
+                                <td>{"$".repeat(restaurant.price_range)}</td> 
+                                <td>reviews</td>
+                                <td>
+                                    <button className="btn btn-warning">Update</button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-danger">Delete</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
 
-                })} */}
 
+                    {/* <tr>
+                        <td>mcdonalds</td>
+                        <td>New York</td>
+                        <td>$$</td>
+                        <td>Rating</td>
+                        <td><button className="btn btn-warning">Update</button></td>
+                        <td><button className="btn btn-danger">Delete</button></td>
+                    </tr>
 
-                <tr>
-                    <td>mcdonalds</td>
-                    <td>New York</td>
-                    <td>$$</td>
-                    <td>Rating</td>
-                    <td><button className="btn btn-warning">Update</button></td>
-                    <td><button className="btn btn-danger">Delete</button></td>
-                </tr>
+                    <tr>
+                        <td>mcdonalds</td>
+                        <td>New York</td>
+                        <td>$$</td>
+                        <td>Rating</td>
+                        <td><button className="btn btn-warning">Update</button></td>
+                        <td><button className="btn btn-danger">Delete</button></td>
+                    </tr> */}
+                </tbody>
 
-                <tr>
-                    <td>mcdonalds</td>
-                    <td>New York</td>
-                    <td>$$</td>
-                    <td>Rating</td>
-                    <td><button className="btn btn-warning">Update</button></td>
-                    <td><button className="btn btn-danger">Delete</button></td>
-                </tr>
-            </tbody>
         </table>
         
     </div>
