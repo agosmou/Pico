8ca30ab7c9fa65946252f6aa95507158a21fdf93
ale_WSL2 <agosmou@gmail.com>
2022-11-26 18:10:23 -0800

    added functionality to show average rating on the home page

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useContext} from 'react';
 import RestaurantFinder from '../apis/RestaurantFinder';
 import { RestaurantsContext } from '../context/RestaurantsContext';
 import { useNavigate} from "react-router-dom"; //useHistory no longer supported
+import StarRating from './StarRating';
 
 
 
@@ -12,6 +13,7 @@ const RestaurantList = (props) => {
       const fetchData = async () => {
         try {
           const response = await RestaurantFinder.get("/");
+          console.log(response.data.data);
           setRestaurants(response.data.data.restaurants);
         } catch (err) {}
       };
@@ -42,6 +44,19 @@ const handleUpdate = (e, id) => {
     navigate(`/restaurants/${id}`);
   };
 
+  const renderRating = (restaurant) => {
+    if (!restaurant.count) {
+      return <span className="text-warning">0 reviews</span>;
+    }
+    return (
+      <>
+        <StarRating rating={restaurant.id} />
+        <span className="text-warning ml-1">({restaurant.count})</span>
+      </>
+    );
+  };
+
+
   return (
     <div className="list-group">
         <table className="table table-hover table-dark">
@@ -67,7 +82,7 @@ const handleUpdate = (e, id) => {
                                 <td>{restaurant.name}</td>
                                 <td>{restaurant.location}</td>
                                 <td>{"$".repeat(restaurant.price_range)}</td> 
-                                <td>reviews</td>
+                                <td>{renderRating(restaurant)}</td>
                                 <td>
                                     <button onClick={(e) => handleUpdate
                                     (e, restaurant.id)}
